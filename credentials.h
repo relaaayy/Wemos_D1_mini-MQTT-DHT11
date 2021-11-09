a0182cd531fc166cedcdac225dcd09735d2d5ade
relaychris <info@christianlau.de>
2021-11-09 23:14:08 +0100

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,8 @@
+// WiFi-Settings SSID and Key
+const char* ssid = "xxx";
+const char* password = "xxx";
+
+// MQTT-Settings Server, User and Password
+#define mqtt_server "xxx.xx.xxx.xxx"
+#define mqtt_user "xxx"
+#define mqtt_password "xxx"
